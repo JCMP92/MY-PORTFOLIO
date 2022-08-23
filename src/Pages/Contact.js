477507f2ce1590d89fb19e6162d741174bcf7b5d
JCMP92 <arq_juanmp@outlook.com>
2022-08-23 14:51:38 -0500

    Added EmailJS to contact component

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,7 +1,31 @@
+import './Contact.css';
+import ContactUs from '../Components/ContactForm';
+
 function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
+      <ContactUs />
+      <div>
+        <a
+          href="https://www.linkedin.com/in/juan-carlos-montes-pesquera-747271242/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+            alt="Linkedin"
+            height="80px"
+          />
+        </a>
+        <a href="https://github.com/JCMP92" target="_blank" rel="noreferrer">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            alt="Github"
+            height="80px"
+          />
+        </a>
+      </div>
     </div>
   );
 }
