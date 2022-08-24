8f4a74f0e4cc503c982b889c18a44d183327b74d
JCMP92 <arq_juanmp@outlook.com>
2022-08-24 11:33:26 -0500

    changing to a mobile first aproach

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,24 @@
 import { Link } from 'react-router-dom';
-import logo from '../Img/Juan Montes-Pesquera-B.png';
+import logo from '../Img/juan-logo.png';
+
 function Navbar() {
   return (
-    <div>
-      <div>
-        <h2>
+    <div className="flex justify-between bg-zinc-800 h-1/6 px-5">
+      <div className="flex items-center hover:scale-105">
+        <div>
           <Link to="/">
-            <img src={logo} alt="logo-juan" height="100px" />
+            <img src={logo} alt="logo-juan" className="h-28" />
           </Link>
-        </h2>
+        </div>
       </div>
-      <div>
-        <h2>
+      <div className="flex justify-between items-center space-x-5 text-xl">
+        <h2 className="text-slate-200 hover:scale-110">
           <Link to="/about">About</Link>
         </h2>
-        <h2>
+        <h2 className="text-slate-200 hover:scale-110">
           <Link to="/projects">Projects</Link>
         </h2>
-        <h2>
+        <h2 className="text-slate-200 hover:scale-110">
           <Link to="/contact">Contact</Link>
         </h2>
       </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <h2>Hello World!</h2>
-      <h1>I´m Juan Montes-Pesquera</h1>
-      <h1>A Front End Developer</h1>
-      <h1>
-        <Link to="/about">Get to know me!</Link>
-      </h1>
+    <div className="flex flex-col justify-evenly items-center h-5/6 px-10 py-5 text-6xl">
+      <div className="w-3/5 space-y-10">
+        <h1 className="text-left">Hello World!</h1>
+        <h1 className="text-left ml-6">I´m Juan Montes-Pesquera</h1>
+        <h1 className="text-left">A Front End Developer</h1>
+        <h1 className="animate-bounce text-orange-600 ">
+          <Link to="/about">Get to know me!</Link>
+        </h1>
+      </div>
     </div>
   );
 }
